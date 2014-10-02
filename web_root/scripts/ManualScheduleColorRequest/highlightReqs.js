32fd047befe6a53124261104357a33bce3950056
Nathan Jones <nathan.jones@ironmail.org>
2014-10-02 16:28:04 -0600

    Initial commit with contributors

---
+++ web_root/scripts/ManualScheduleColorRequest/highlightReqs.js
@@ -0,0 +1,46 @@
+/*global $j*/
+(function () {
+    'use strict';
+    var template = $j($j('#color-key-template').html());
+    var select = $j('.box-round');
+
+    template.insertBefore(select);
+
+    var psData = $j.parseJSON($j('#ps-data').data().ps);
+
+    psData.scheduleRequests.splice(-1, 1);
+    var requests = psData.scheduleRequests;
+
+    var numRequests = requests.length - 1;
+    var numRows = $j('.box-round tr').length;
+
+    //Loop through the table rows
+    for (var i = 3; i < numRows; i++) {
+        var courseNum = $j('.box-round tr')[i].children[0].innerHTML;
+
+        //Loop through the requests to find a match
+        for (var j = 0; j < numRequests; j++) {
+            //This is the request that matches the table row
+            if (requests[j].course === courseNum) {
+
+                //It's a PRIMARY request -- color it green
+                if (requests[j].type === '') {
+                    $j('.box-round tr')[i].children[0].style.setProperty('background-color', '#00f000', '');
+                    $j('.box-round tr')[i].children[0].innerHTML += '&nbsp;&nbsp;(' + requests[j].priority + ')';
+                }
+                //It's an ELECTIVE (non-alt) -- color it yellow
+                else if (requests[j].type === 'E' && requests[j].alternate === 0) {
+                    $j('.box-round tr')[i].children[0].style.setProperty('background-color', '#f0f000', '');
+                    $j('.box-round tr')[i].children[0].innerHTML += '&nbsp;&nbsp;(' + requests[j].priority + ')';
+                }
+
+                //It's an ALT ELECTIVE -- color it red
+                else if (requests[j].type === 'E' && requests[j].alternate === 1) {
+                    $j('.box-round tr')[i].children[0].style.setProperty('background-color', '#f00000', '');
+                    $j('.box-round tr')[i].children[0].innerHTML += '&nbsp;&nbsp;(' + requests[j].priority + ')';
+                }
+            }
+        }
+    }
+
+}());
