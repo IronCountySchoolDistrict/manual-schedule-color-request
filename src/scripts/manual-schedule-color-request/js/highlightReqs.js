e3ca343b75adcbf756412138ea5a2681176a27f5
basiczombie <issacgable@gmail.com>
2017-01-05 09:00:16 -0700

    now conforms to es2015

--- src/scripts/manual-schedule-color-request/js/highlightReqs.js
+++ src/scripts/manual-schedule-color-request/js/highlightReqs.js
@@ -1,46 +1,47 @@
-/*global $j*/
-(function () {
-    'use strict';
-    var template = $j($j('#color-key-template').html());
-    var select = $j('.box-round');
+import $ from "jquery";
 
-    template.insertBefore(select);
+export default function() {
+    $(() => {
+        var template = $($('#color-key-template').html());
+        var select = $('.box-round');
 
-    var psData = $j.parseJSON($j('#ps-data').data().ps);
+        template.insertBefore(select);
 
-    psData.scheduleRequests.splice(-1, 1);
-    var requests = psData.scheduleRequests;
+        var psData = $.parseJSON($('#ps-data').data().ps);
 
-    var numRequests = requests.length - 1;
-    var numRows = $j('.box-round tr').length;
+        psData.scheduleRequests.splice(-1, 1);
+        var requests = psData.scheduleRequests;
 
-    //Loop through the table rows
-    for (var i = 3; i < numRows; i++) {
-        var courseNum = $j('.box-round tr')[i].children[0].innerHTML;
+        var numRequests = requests.length - 1;
+        var numRows = $('.box-round tr').length;
 
-        //Loop through the requests to find a match
-        for (var j = 0; j < numRequests; j++) {
-            //This is the request that matches the table row
-            if (requests[j].course === courseNum) {
+        //Loop through the table rows
+        for (var i = 3; i < numRows; i++) {
+            var courseNum = $('.box-round tr')[i].children[0].innerHTML;
 
-                //It's a PRIMARY request -- color it green
-                if (requests[j].type === '') {
-                    $j('.box-round tr')[i].children[0].style.setProperty('background-color', '#00f000', '');
-                    $j('.box-round tr')[i].children[0].innerHTML += '&nbsp;&nbsp;(' + requests[j].priority + ')';
-                }
-                //It's an ELECTIVE (non-alt) -- color it yellow
-                else if (requests[j].type === 'E' && requests[j].alternate === 0) {
-                    $j('.box-round tr')[i].children[0].style.setProperty('background-color', '#f0f000', '');
-                    $j('.box-round tr')[i].children[0].innerHTML += '&nbsp;&nbsp;(' + requests[j].priority + ')';
-                }
+            //Loop through the requests to find a match
+            for (var j = 0; j < numRequests; j++) {
+                //This is the request that matches the table row
+                if (requests[j].course === courseNum) {
 
-                //It's an ALT ELECTIVE -- color it red
-                else if (requests[j].type === 'E' && requests[j].alternate === 1) {
-                    $j('.box-round tr')[i].children[0].style.setProperty('background-color', '#f00000', '');
-                    $j('.box-round tr')[i].children[0].innerHTML += '&nbsp;&nbsp;(' + requests[j].priority + ')';
+                    //It's a PRIMARY request -- color it green
+                    if (requests[j].type === '') {
+                        $('.box-round tr')[i].children[0].style.setProperty('background-color', '#00f000', '');
+                        $('.box-round tr')[i].children[0].innerHTML += '&nbsp;&nbsp;(' + requests[j].priority + ')';
+                    }
+                    //It's an ELECTIVE (non-alt) -- color it yellow
+                    else if (requests[j].type === 'E' && requests[j].alternate === 0) {
+                        $('.box-round tr')[i].children[0].style.setProperty('background-color', '#f0f000', '');
+                        $('.box-round tr')[i].children[0].innerHTML += '&nbsp;&nbsp;(' + requests[j].priority + ')';
+                    }
+
+                    //It's an ALT ELECTIVE -- color it red
+                    else if (requests[j].type === 'E' && requests[j].alternate === 1) {
+                        $('.box-round tr')[i].children[0].style.setProperty('background-color', '#f00000', '');
+                        $('.box-round tr')[i].children[0].innerHTML += '&nbsp;&nbsp;(' + requests[j].priority + ')';
+                    }
                 }
             }
         }
-    }
-
-}());
+    });
+}
